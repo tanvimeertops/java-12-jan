88fd23a62334d6b91bfa1d37980b0d890c837d27
sdksd <tanvimeer.tops@gmail.com>
2026-05-22 12:11:30 +0530

    22 may

---
+++ src/com/basic/AssignmentOperator.java
@@ -0,0 +1,18 @@
+package com.basic;
+/*
+ * = 
+ * shorthand
+ * +=
+ * -=
+ * *=
+ */
+public class AssignmentOperator {
+public static void main(String[] args) {
+	int salary=120000;
+	
+	System.out.println("salary is "+salary);
+	salary-=50000;
+	System.out.println("revised salary is "+salary);
+	
+}
+}
